9cdf286b4cc8fcd58c8b81ef1d76ac9b31fa90e7
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-21 00:11:59 +0530

    emojis made larger

--- master.js
+++ master.js
@@ -1,10 +1,13 @@
+let angry = document.getElementById('angry')
+let happy = document.getElementById('happy')
+let sad = document.getElementById('sad')
+let crying = document.getElementById('crying')
+let upset = document.getElementById('upset')
+let love = document.getElementById('love')
+let frustrated = document.getElementById('frustrated')
+let annoyed = document.getElementById('annoyed')
+
 const moodClicked = function (mood) {
-    let angry = document.getElementById('angry')
-    let happy = document.getElementById('happy')
-    let sad = document.getElementById('sad')
-    let crying = document.getElementById('crying')
-    let upset = document.getElementById('upset')
-    let love = document.getElementById('love')
 
     angry.classList.replace('fa-solid', 'fa-regular')
     happy.classList.replace('fa-solid', 'fa-regular')
@@ -12,6 +15,8 @@ const moodClicked = function (mood) {
     crying.classList.replace('fa-solid', 'fa-regular')
     upset.classList.replace('fa-solid', 'fa-regular')
     love.classList.replace('fa-solid', 'fa-regular')
+    frustrated.classList.replace('fa-solid', 'fa-regular')
+    annoyed.classList.replace('fa-solid', 'fa-regular')
     
     let element = mood.id
     feeling = document.getElementById(element)
